4ac6870d6afd82a7ab706ec4384a69263bc8de6a
Netflix-Clone <yashvishrest@gmail.com>
2026-04-21 20:46:33 +0530

    backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+//   credentials: true
+// }));
+app.use(cors());
 
 app.use(express.json());
 
